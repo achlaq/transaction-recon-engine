e517374289632955d232cdf23b29c0ec165367e7
Achlaq Al Arif <achlaqalarif@yahoo.co.id>
2025-12-12 10:39:11 +0700

    chore: init the spring boot project

---
+++ src/main/java/achlaq/co/transactionreconengine/TransactionReconEngineApplication.java
@@ -0,0 +1,13 @@
+package achlaq.co.transactionreconengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionReconEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionReconEngineApplication.class, args);
+    }
+
+}
